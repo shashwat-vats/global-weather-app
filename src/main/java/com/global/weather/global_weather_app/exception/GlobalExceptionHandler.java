c8a6236f9a00f6b6ed806abe573d0d6fc9cecec7
shashwat-vats <shashwat.official@outlook.com>
2024-08-22 22:01:04 +0530

    Exception handler class added

---
+++ src/main/java/com/global/weather/global_weather_app/exception/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.global.weather.global_weather_app.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+}
